03ca1b325c01eef10719281be71f283f8d2630cf
Lorençone <mmlorencone@gmail.com>
2018-11-19 11:27:02 -0200

    Inserido um novo dump e alterado o nome do dump antigo

---
+++ banco de dados/Filmes_Atualizados/filmes_filme.sql
@@ -0,0 +1,68 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: filmes
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.36-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `filme`
+--
+
+DROP TABLE IF EXISTS `filme`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filme` (
+  `id_filme` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+  `estreia` date NOT NULL,
+  `estudio` varchar(50) COLLATE utf8_czech_ci DEFAULT NULL,
+  `bilheteria` int(11) DEFAULT NULL,
+  `duracao` time NOT NULL,
+  `sinopse` text COLLATE utf8_czech_ci NOT NULL,
+  `critica` text COLLATE utf8_czech_ci,
+  `trailer` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+  `assistir` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+  `indicacao` int(11) DEFAULT NULL,
+  `gasto` int(11) DEFAULT NULL,
+  `imagem` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `id_classificacao` int(11) NOT NULL,
+  `id_pais` int(11) NOT NULL,
+  PRIMARY KEY (`id_filme`),
+  KEY `fk_filme_classificacao1_idx` (`id_classificacao`),
+  KEY `fk_filme_pais1_idx` (`id_pais`),
+  CONSTRAINT `fk_filme_classificacao1` FOREIGN KEY (`id_classificacao`) REFERENCES `classificacao` (`id_classificacao`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_filme_pais1` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id_pais`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filme`
+--
+
+LOCK TABLES `filme` WRITE;
+/*!40000 ALTER TABLE `filme` DISABLE KEYS */;
+INSERT INTO `filme` VALUES (1,'As Branquelas','1111-01-16','Pixar',12121212,'00:01:20','asdasd','asdasd','https://www.youtube.com/watch?v=aFO_zBptWSM','https://www.youtube.com/watch?v=fCqx_7J5Qx0',2,1212121,'branquelas.jpg',1,149);
+/*!40000 ALTER TABLE `filme` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-19 11:23:49
---
+++ banco de dados/Filmes_Atualizados/filmes_filme_genero.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: filmes
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.36-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `filme_genero`
+--
+
+DROP TABLE IF EXISTS `filme_genero`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filme_genero` (
+  `id_filme` int(11) NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  PRIMARY KEY (`id_filme`,`id_genero`),
+  KEY `fk_filme_has_genero_genero1_idx` (`id_genero`),
+  KEY `fk_filme_has_genero_filme1_idx` (`id_filme`),
+  CONSTRAINT `fk_filme_has_genero_filme1` FOREIGN KEY (`id_filme`) REFERENCES `filme` (`id_filme`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_filme_has_genero_genero1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filme_genero`
+--
+
+LOCK TABLES `filme_genero` WRITE;
+/*!40000 ALTER TABLE `filme_genero` DISABLE KEYS */;
+INSERT INTO `filme_genero` VALUES (1,1),(1,2);
+/*!40000 ALTER TABLE `filme_genero` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-19 11:23:51
