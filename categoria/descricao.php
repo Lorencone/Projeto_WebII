8697e063506cf013761044ffcb572a4d39e7d619
Lorençone <mmlorencone@gmail.com>
2018-11-19 11:37:56 -0200

    Faltando terminar o crude de descrição.

--- categoria/descricao.php
+++ categoria/descricao.php
@@ -9,20 +9,20 @@ include_once '../cabecalho.php';
 
 <div class="container">
     <h1><?= $filme->getNome()?></h1>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
+    <br>
+    <p><strong>Estreia: </strong><?= $filme->getEstreia();?></p>
+    <p><strong>Estudio: </strong><?= $filme->getEstudio();?></p>
+    <p><strong>Bilheteria: </strong><?= $filme->getBilheteria();?></p>
+    <p><strong>Duracao: </strong><?= $filme->getDuracao();?></p>
+    <p><strong>Sinopse: </strong><?= $filme->getSinopse();?></p>
+    <p><strong>Critica: </strong><?= $filme->getCritica();?></p>
+    <p><strong>Trailer: </strong><?= $filme->getTrailer();?></p>
+    <p><strong>Assistir: </strong><?= $filme->getAssistir();?></p>
+    <p><strong>Indicacao: </strong><?= $filme->getIndicacao();?></p>
+    <p><strong>Gasto: </strong><?= $filme->getGasto();?></p>
+    <p><strong>Imagem: </strong><?= $filme->getImagem();?></p>
+    <p><strong>Classificacao: </strong><?= $filme->getIdClassificacao();?></p>
+    <p><strong>Pais: </strong><?= $filme->getIdPais();?></p>
 </div>
 
 <?php
